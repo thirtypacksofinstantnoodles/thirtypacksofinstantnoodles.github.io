77fa9db43d51478b4fb678cb43deecf4be432fd4
kendarik <167174226+kendarik@users.noreply.github.com>
2025-08-04 20:28:00 +0800

    Create feedbackfirebase.js

---
+++ feedbackfirebase.js
@@ -0,0 +1,20 @@
+// Firebase CDN module imports
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-analytics.js";
+
+// Your Firebase config (already created in Firebase console)
+const firebaseConfig = {
+  apiKey: "AIzaSyBtJnZ7ZK8JW1FH6gd1SPKJoJXJMSgzpM4",
+  authDomain: "instant-noodles-a8c7d.firebaseapp.com",
+  projectId: "instant-noodles-a8c7d",
+  storageBucket: "instant-noodles-a8c7d.firebasestorage.app",
+  messagingSenderId: "785588231065",
+  appId: "1:785588231065:web:17f9866a7e7960613a1108",
+  measurementId: "G-T6L1X7NGGD"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+console.log("Firebase initialized", app);
